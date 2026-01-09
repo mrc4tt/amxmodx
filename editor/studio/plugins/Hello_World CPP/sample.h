158f5287626d3bd06d1a2f1b505d1a254ccdc66f
mrc4tt <mikkel.linddk@gmail.com>
2026-01-09 23:41:28 +0100

    .

--- editor/studio/plugins/Hello_World CPP/sample.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MYPLUGIN_INCLUDE_H
-#define _MYPLUGIN_INCLUDE_H
-
-#include "studio_api.h"
- 
-EXPORT void PluginLoad(load_info *pInfo);
-EXPORT void PluginUnload();
-
-#endif //_MYPLUGIN_INCLUDE_H
